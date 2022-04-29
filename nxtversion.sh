678e1b1046ee72fc7eda02c30a0255b95f79e02a
Mohit Garg <mgarg51@bloomberg.net>
2022-04-29 10:49:54 -0400

    Minor changes

--- nxtversion.sh
+++ nxtversion.sh
@@ -2,10 +2,7 @@
 last_tag=$(git tag -l | head -1 | egrep "^[0-9]+.[0-9]+.[0-9]+$")
 
 patch=$(echo $last_tag | cut -d "." -f3)
-version=$(echo $last_tag | cut -d "." -f1 -f2)
-
-echo $patch
-echo $version
+version=$(echo $last_tag | cut -d "." -f1,2)
 
 patch=`expr $patch + 1`
 next_version="$version.$patch"
